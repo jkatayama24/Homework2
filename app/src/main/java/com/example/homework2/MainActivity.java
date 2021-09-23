50850b9be30cc0632be8631c1219c573fedfc822
kkata <katayama24@up.edu>
2021-09-22 19:01:56 -0700

    Starting project and making gameview

---
+++ app/src/main/java/com/example/homework2/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.homework2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * Square puzzle game
+ *
+ * @author Jason Katayama
+ * @version
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/homework2/PuzzleView.java
@@ -0,0 +1,5 @@
+package com.example.homework2;
+
+public class PuzzleView {
+
+}
